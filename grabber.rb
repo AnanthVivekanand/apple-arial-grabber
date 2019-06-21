09805deef3e51651eae7733cdffa21f5ec7eddb3
KorkyMonster <avivek3021@gmail.com>
2019-06-21 13:48:05 +0530

    Basic Y/n input

--- grabber.rb
+++ grabber.rb
@@ -1,15 +1,28 @@
 require 'net/http'
 require 'json'
+require 'highline/import'
+
+
+def yesno(prompt = 'Continue?', default = true)
+  a = ''
+  s = default ? '[Y/n]' : '[y/N]'
+  d = default ? 'y' : 'n'
+  until %w[y n].include? a
+    a = ask("#{prompt} #{s} ") { |q| q.limit = 1; q.case = :downcase }
+    a = d if a.length == 0
+  end
+  a == 'y'
+end
 
 class Grabber
   attr_accessor :save_location
 
-  APPLE_URL = "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json"                                                                                                                                                                      
+  APPLE_URL = "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json"
 
   def initialize
     @save_location = "."
   end
-
+  
   def download_json
     uri = URI(APPLE_URL)
     response = Net::HTTP.get(uri)
@@ -22,6 +35,7 @@ class Grabber
       video_index = item['assets']
       video_index.each do |video|
         puts "  Found #{video['id']}, place: #{video['accessibilityLabel']}, #{video['timeOfDay']}time"
+        puts yesno ("Download?")
       end
     end
   end
